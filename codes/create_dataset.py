3f26e60e74b34c1b4ae3bbfdcfe45b5a9cbf9c62
khoa nguyen dang <ndkhoa0704@gmail.com>
2023-07-18 10:16:20 +0700

    fix

--- codes/create_dataset.py
+++ codes/create_dataset.py
@@ -1,24 +1,31 @@
 import json
 import argparse
 import os
+import shutil
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='wn18rr')
 
 
 def prepare_data(path: str):
-    filename = path.split('/')[-1]
-    new_filenname = filename.split('.')[0] + '.txt'
+    file_list = os.listdir(path)
+    jsonfiles = filter(lambda x: x.endswith('.txt.json'), file_list)
+    if 'new_data' in file_list:
+        new_path = os.path.join(path, 'new_data')
+        shutil.rmtree(new_path)
+        os.mkdir(new_path)
 
-    new_path = '/'.join(path.split('/')[:-1]) + '/new_data/'
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
-    
-    new_path += new_filenname
-    json_data = json.load(open(path))
-    with open(new_path ,'w') as f:
-        for jdata in json_data:
-            f.write(f"{jdata['head_id']}\t{jdata['relation']}\t{jdata['tail_id']}\t{jdata['bert_h_id']}\t{jdata['bert_t_id']}\n")
+    for file in file_list:
+        if file.endswith(('.txt',' json', '.dict')):
+            shutil.copy(os.path.join(path, file), os.path.join(new_path, file))
+
+    for jsonfile in jsonfiles:
+        json_data = json.load(open(os.path.join(path, jsonfile)))
+        with open(os.path.join(new_path, jsonfile.split('.')[0] + '.txt') ,'w') as f:
+            for jdata in json_data:
+                f.write(f"{jdata['head_id']}\t{jdata['relation']}\t{jdata['tail_id']}\t{jdata['bert_h_id']}\t{jdata['bert_t_id']}\n")
+
         
 
 if __name__=='__main__':
@@ -29,11 +36,6 @@ if __name__=='__main__':
     if dataset not in ['wn18rr', 'fb15k237']:
         raise Exception('Dataset not supported. Supported datasets are: wn18rr, fb15k237')
 
-    paths = [
-        f'../data/{dataset}/train.txt.json',
-        f'../data/{dataset}/test.txt.json',
-        f'../data/{dataset}/valid.txt.json'
+    path = f'../data/{dataset}'
 
-    ]
-    for path in paths:
-        prepare_data(path)
+    prepare_data(path)
